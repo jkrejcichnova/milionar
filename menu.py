8f87d0dfee8cfca074d24239d5c59787c1c65cc7
Jan Krejčí <st025509@student.cichnovabrno.cz>
2025-12-04 10:57:17 +0100

    refactor(MENU): added boilerplate menu

--- menu.py
+++ menu.py
@@ -1,3 +1,28 @@
+def boilerplate():
+    exit = False
+    while not exit:
+        menu: str = f"TITLE:\n1) Option\n2) Option\n3) Exit\n"
+        input_char: str = f"> "
+        user_input_raw: str = input(menu+input_char).strip().lower()
+        if not user_input_raw.isdigit():
+            user_input = -1
+        else:
+            user_input: int = int(user_input_raw)
+        try:
+            match user_input:
+                case 1:
+                    print()
+                    # TODO
+                case 2:
+                    print()
+                    #  TODO
+                case 3:
+                    exit = True
+                case _:
+                    raise ValueError("Incorrect menu option.")
+        except ValueError as e:
+            print(f"Error: {e}")
+
 def main():
     exit = False
     while not exit:
@@ -16,3 +41,4 @@ def main():
                 # TODO
             case 3:
                 exit = True
+            
