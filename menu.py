20f9fb41d80603f6e43fcac6135b31e7dad40520
Jan Krejčí <st025509@student.cichnovabrno.cz>
2025-12-04 10:58:29 +0100

    fix(MENU): updated menu according to boilerplate

--- menu.py
+++ menu.py
@@ -29,16 +29,22 @@ def main():
         menu: str = f"Welcome! Would you like to:\n1) Log-In\n2) Register\n3) Exit\n"
         input_char: str = f"> "
         user_input_raw: str = input(menu+input_char).strip().lower()
-        if not user_input_raw.isdigit:
-            user_input_raw = -1
-        user_input: int = int(user_input_raw)
-        match user_input:
-            case 1:
-                print()
-                # TODO
-            case 2:
-                print()
-                # TODO
-            case 3:
-                exit = True
+        if not user_input_raw.isdigit():
+            user_input = -1
+        else:
+            user_input: int = int(user_input_raw)
+        try:
+            match user_input:
+                case 1:
+                    print()
+                    # TODO
+                case 2:
+                    print()
+                    #  TODO
+                case 3:
+                    exit = True
+                case _:
+                    raise ValueError("Incorrect menu option.")
+        except ValueError as e:
+            print(f"Error: {e}")
             
