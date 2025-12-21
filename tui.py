8dc61f2286f216e7aec9e7eeeead38f7aebf0863
Jan Krejčí <st025509@student.cichnovabrno.cz>
2025-12-21 21:41:08 +0100

    feat: add statistics

--- tui.py
+++ tui.py
@@ -1,7 +1,12 @@
+import re
+from typing import Any, Optional
 import auth
 from user import User
 import game
+from game_database import get_results
 from graph import q_categories, q_difficulties
+from result import UserResults, GlobalResults
+from user_database import get_database
 
 def menu_boilerplate():
     exit = False
@@ -27,6 +32,39 @@ def menu_boilerplate():
                     raise ValueError("Incorrect menu option.")
         except ValueError as e:
             print(f"Error: {e}")
+
+def get_output_lengths(l: list[Any]) -> list[int]:
+    list_str: list[int] = []
+    for i in l:
+        list_str.append(len(str(i)))
+    return list_str
+      
+
+def output_dict_as_table(d: dict, title: str | None = None, headers: tuple[str,str] | None = None) -> str:
+    output = ""
+    keys: list[Any] = list(d.keys())
+    keys.append(0)
+    values: list[Any] = list(d.values())
+    values.append(0)
+    width_col1 = max(get_output_lengths(keys))
+    width_col2 = max(get_output_lengths(values))
+    if headers != None:
+        width_col1 = max([width_col1, len(headers[0])])
+        width_col2 = max([width_col2, len(headers[1])])
+    padding = 1
+    col_width = max([width_col1, width_col2, len(str(title))]) + padding*2
+    total_inner_width = col_width*2+2
+    if title != None:
+        output += f"╔{total_inner_width*"═"}╗\n"
+        output += f"║{title.center(total_inner_width)}║\n"
+        output += f"╚{total_inner_width*"═"}╝\n"
+    if headers:
+        output += f" {headers[0].center(col_width)} {headers[1].center(col_width)} \n"
+    output += f"╭{col_width*"─"}┬{col_width*"─"}╮\n"
+    for i in d.items():
+        output += f"│{str(i[0]).center(col_width)}│{str(i[1]).center(col_width)}│\n"
+    output += f"╰{col_width*"─"}┴{col_width*"─"}╯\n"
+    return output
 
 def valid_input(prompt: str, validator_function, input_char: str|None = ": ") -> str:
     while True:
@@ -72,10 +110,58 @@ def login() -> User|None:
         return None
     return user
 
+def top_dict(d: dict[Any, Any], top=5, r=False) -> dict[Any, Any]:
+    sorted_keys = sorted(d, key=d.get, reverse=r)
+    top_keys = sorted_keys[:min(top, len(d))]
+    final = {}
+    for key in top_keys:
+        final[key] = d[key]    
+    return final
+
+def replace_id_with_name_in_dict(d: dict[int, Any]) -> dict[str, Any]:
+    database = get_database()
+    result: dict[str, Any] = {}
+    for item in tuple(d.items()):
+        result[database.get_username_from_id(item[0])] = item[1]
+    return result
+
+def view_user_stats(user: User):
+    global_stats: GlobalResults = GlobalResults(get_results())
+    stats: UserResults = global_stats.get_user_games(user.id)
+    winrate = stats.winrate()*100
+    print(output_dict_as_table({
+        "Games won": stats.games_won(),
+        "Games total": stats.games_total(),
+        "Winrate": f"{round(winrate, 2)}%",
+        "  ": "",
+        "Total correct questions": stats.total_achieved(),
+        "Average questions per game": f"{round(stats.average_achieved(), 2)}",
+        "Highest score": stats.high_score(),
+        "   ": "",
+        "Average time per game": f"{round(stats.average_time_per_game(), 2)}s",
+        "Fastest won game": f"{round(stats.fastest_game(), 2)}s",
+        "Total time spent playing": f"{round(stats.total_time(), 2)}s",
+    }, title=f"{user.username}'s statistics"))
+
+def view_global_stats():
+    global_stats: GlobalResults = GlobalResults(get_results())
+    database = get_database()
+    total_q = global_stats.highest_achieved()
+    fastest_g = global_stats.fastest_game()
+    most_games = global_stats.most_games_played()
+    print(output_dict_as_table({
+        "Highest total questions answered correctly": f"{database.get_username_from_id(total_q[0])} with {total_q[1]} questions.",
+        "Most games played": f"{database.get_username_from_id(most_games[0])} with {most_games[1]} games"
+    }, title=f"World records"))
+    print(output_dict_as_table(replace_id_with_name_in_dict(top_dict(global_stats.games_won())), title="Most games won"))
+    print(output_dict_as_table(replace_id_with_name_in_dict(top_dict(global_stats.fastest_wins())), title="Fastest wins"))
+
+
+
 def logged_in_menu(user: User):
     exit = False
     while not exit:
-        menu: str = f"Hello {user.username}:\n1) View your game statistics\n2) List all winners\n3) Play \"Cichna wants to be a millionare\"\n4) Exit\n"
+        menu: str = f"Hello {user.username}:\n1) View your statistics\n2) View global statistics\n3) Play \"Cichna wants to be a millionare\"\n4) Exit\n"
         input_char: str = f"> "
         user_input_raw: str = input(menu+input_char).strip().lower()
         if not user_input_raw.isdigit():
@@ -85,11 +171,9 @@ def logged_in_menu(user: User):
         try:
             match user_input:
                 case 1:
-                    print("View your stats")
-                    # TODO
+                    view_user_stats(user)
                 case 2:
-                    print("List all winners")
-                    #  TODO
+                    view_global_stats()
                 case 3:
                     print(game.launch(user))
                 case 4:
@@ -113,10 +197,8 @@ def graph_menu():
             match user_input:
                 case 1:
                     q_difficulties()
-                    # TODO
                 case 2:
                     q_categories()
-                    #  TODO
                 case 3:
                     exit = True
                 case _:
@@ -142,11 +224,9 @@ def main_menu():
                         continue
                     print(user)
                     logged_in_menu(user)
-                    # TODO
                 case 2:
                     user = register()
                     logged_in_menu(user)
-                    # TODO
                 case 3:
                     graph_menu()
                 case 4:
@@ -155,4 +235,3 @@ def main_menu():
                     raise ValueError("Incorrect menu option.")
         except ValueError as e:
             print(f"Error: {e}")
-            
